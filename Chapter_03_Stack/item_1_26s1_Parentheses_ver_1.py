5e18155d4f4d0111635cad623922cd687013c323
Chawanuea <55906160+chawannua@users.noreply.github.com>
2026-08-10 12:32:00 +0700

    Update item_1_26s1_Parentheses_ver_1.py

--- Chapter_03_Stack/item_1_26s1_Parentheses_ver_1.py
+++ Chapter_03_Stack/item_1_26s1_Parentheses_ver_1.py
@@ -3,9 +3,9 @@
 # --------------------------------------------------------------------------------
 # Problem Statement:
 #     Write a program to receive input in the form of brackets. 
-      The opening brackets are: ( and [ and the closing brackets are: ) and ]. 
+#     The opening brackets are: ( and [ and the closing brackets are: ) and ]. 
 #     Determine if the brackets can be paired correctly.    
-      Display the number of brackets needed to complete the pairs if they are incomplete.     If all pairs are complete, display "Perfect".
+#     Display the number of brackets needed to complete the pairs if they are incomplete.     If all pairs are complete, display "Perfect".
 # ================================================================================
 
 class Stack:
